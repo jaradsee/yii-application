d4d2fd78511d42cf08602e8d2d54b1439b1e0a9e
john <john@john-PC>
2015-06-12 12:45:11 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,10 +1,21 @@
 <?php
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'โรงพยาบาลฟากท่า';
 ?>
-<div class="site-index">
+<?php
+$route1 = Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>"> ไปที่ test1</a>
+<br>
+    
+<?php
+$route2 = Yii::$app->urlManager->createUrl(['test/test2','name'=>'jarad','lname'=>'seeka']);
+?>
+<a href="<?=$route2?>"> ไปที่ test2</a>
+<br>
 
-    <div class="jumbotron">
-        <h1>ยินดีต้อรับท่านเข้าสู่เว็บไซต์โรงพยาบาลฟากท่า</h1>
+<?=
+\yii\helpers\Html::a('ลิงค์แบบที่ 3',['test/test1','a'=>'1']);
 
-        
+?>
